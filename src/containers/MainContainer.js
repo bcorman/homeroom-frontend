ff6cde1ecd5b92efdd6d53c6b83b7e0c66fdf12d
bcorman <benjamincorman@gmail.com>
2018-07-12 13:57:59 -0700

    debugging class container filenames

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import AnnouncementsContainer from './AnnouncementsContainer'
-import classContainer from './classContainer'
+import ClassContainer from './ClassContainer'
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
