1da813709a7e8b2b895a967c614e1713db29d7a6
bcorman <benjamincorman@gmail.com>
2018-07-12 18:23:20 -0700

    debugging urls

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -8,9 +8,7 @@ class MainContainer extends Component {
   render() {
     console.log('in main container')
     console.log(this.props.view)
-    let page
-    if (this.props.view === 'announcements') {
-      page = <AnnouncementsContainer />
+
     } else {
       return (
         <Switch>
@@ -22,6 +20,6 @@ class MainContainer extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { view: state.view.mainContainer }
+  return { view: state.view }
 }
 export default withRouter(connect(mapStateToProps)(MainContainer))
